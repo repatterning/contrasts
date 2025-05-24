5fae06d85522659df229e6ea18fcd5ac3da3ed0e
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-05-24 16:48:33 +0100

    Update src/algorithms/difference.py

--- src/algorithms/difference.py
+++ src/algorithms/difference.py
@@ -1,14 +1,27 @@
-import pandas as pd
+"""Module difference.py"""
 import logging
+
+import pandas as pd
 
 
 class Difference:
+    """
+    Calculates percentage difference between sequential measures, per gauge time series.
+    """
 
     def __init__(self):
+        """
+        Constructor
+        """
 
         self.__periods = 1
 
     def __percentage_change(self, data: pd.DataFrame) -> pd.DataFrame:
+        """
+
+        :param data: Each field represents a distinct gauge.
+        :return:
+        """
 
         frame = data.ffill(axis=0, inplace=False)
         changes = 100 * frame.pct_change(periods=self.__periods, axis=0)
@@ -19,7 +32,7 @@ class Difference:
     def exc(self, data: pd.DataFrame) -> pd.DataFrame:
         """
 
-        :param data: The index field is a datetime field
+        :param data: The index field is a datetime field.  Each field represents a distinct gauge.
         :return:
         """
 
