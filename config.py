5e4af7ebd0ddfc3bf17004efab095d7320cf676f
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-05-25 13:14:43 +0100

    Update config.py

--- config.py
+++ config.py
@@ -29,13 +29,3 @@ class Config:
 
         # The prefix of the Amazon repository where the quantiles will be stored
         self.prefix = 'warehouse/contrasts'
-
-        # Times
-        starting = datetime.datetime.strptime('2024-02-29 00:00:00', '%Y-%m-%d %H:%M:%S')
-        ending = datetime.datetime.strptime('2024-03-01 00:00:00', '%Y-%m-%d %H:%M:%S')
-        timespan = ending - starting
-        timespan.total_seconds()
-
-        self.shift = int(1000 * timespan.total_seconds())
-
-        self.leap = '2024-01-01'
